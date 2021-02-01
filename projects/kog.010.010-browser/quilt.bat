59afa03d7be4f28cdec49496a20caf20186a8187
szecsi <szecsi.laszlo@gmail.com>
2021-02-01 01:11:22 +0100

    code highlighting and floats

---
+++ projects/kog.010.010-browser/quilt.bat
@@ -0,0 +1,5 @@
+rem todo
+rem builds a git repo using a hardcoded list of patches
+rem start: build a git repo if the original
+rem final: build a git repo fo the final
+rem sildes: creates reveal.js slides from the commits  
