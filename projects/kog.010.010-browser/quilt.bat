9266c9d5f950ee35bd819c246ce69b57805448ac
szecsi <szecsi.laszlo@gmail.com>
2021-02-04 11:49:19 +0100

    touchup

--- projects/kog.010.010-browser/quilt.bat
+++ projects/kog.010.010-browser/quilt.bat
@@ -1,5 +1,8 @@
 rem todo
 rem builds a git repo using a hardcoded list of patches
-rem start: build a git repo if the original
-rem final: build a git repo fo the final
+rem start: build a git repo of the original
+rem final: build a git repo of the final
+git clone -b empty --single-branch https://github.com/szecsi/kogedge.git
+git checkout -b quilted
+git apply ../patches/kog.010.010.0010-html.patch
 rem sildes: creates reveal.js slides from the commits  
