8bd2ef109212c82b09c9fc919278e0cfa0e40b46
szecsi <szecsi.laszlo@gmail.com>
2021-02-08 23:37:18 +0100

    highlight del ins

--- dependencies/reveal.js/plugin/floating/floating.js
+++ dependencies/reveal.js/plugin/floating/floating.js
@@ -34,7 +34,7 @@ var updateAnno = function(scrolledbox, maxCharCountPerLine, marginSize, fontSize
                 currentLine += words[iWord] + " ";
               } else {
                 lines.push(currentLine);
-                currentLine = words[iWord];
+                currentLine = words[iWord] + " ";
               }
               iWord++;
             }
@@ -72,18 +72,9 @@ Reveal.on("slidechanged", function(event){
   for(var i=0; i<codeboxes.length; i++){
     var scrolledbox = codeboxes[i];
     updateAnno(scrolledbox, maxCharCountPerLine, marginSize, fontSize);
-  }
-});
-
-Reveal.on("ready", function(event){
-  var maxCharCountPerLine = event.currentSlide.getAttribute("data-maxCharCountPerLine") || 40;
-  var marginSize = event.currentSlide.getAttribute("data-marginSize") || 300;  
-  var fontSize = event.currentSlide.getAttribute("data-fontSize") || 15;
-  var codeboxes = event.currentSlide.querySelectorAll("code.floating-annotations");
-  for(var i=0; i<codeboxes.length; i++){
-    var scrolledbox = codeboxes[i];
     var timeoutId = null;//window.setTimeout(updateAnno, 2000);    
     var delayfunc = function(){
+      console.log("hey scrolled");
       if(timeoutId) {
         var slide = scrolledbox.closest("section");
         var svgs = slide.querySelectorAll("svg");
@@ -94,6 +85,30 @@ Reveal.on("ready", function(event){
       }
       timeoutId = window.setTimeout(function(){updateAnno(scrolledbox, maxCharCountPerLine, marginSize, fontSize);}, 200);
     };
-    scrolledbox.addEventListener("scroll", delayfunc);  
+    scrolledbox.addEventListener("scroll", delayfunc);      
   }
 });
+
+//Reveal.on("ready", function(event){
+//  var maxCharCountPerLine = event.currentSlide.getAttribute("data-maxCharCountPerLine") || 40;
+//  var marginSize = event.currentSlide.getAttribute("data-marginSize") || 300;  
+//  var fontSize = event.currentSlide.getAttribute("data-fontSize") || 15;
+//  var codeboxes = event.currentSlide.querySelectorAll("code.floating-annotations");
+//  for(var i=0; i<codeboxes.length; i++){
+//    var scrolledbox = codeboxes[i];
+//    var timeoutId = null;//window.setTimeout(updateAnno, 2000);    
+//    var delayfunc = function(){
+//      console.log("hey scrolled");
+//      if(timeoutId) {
+//        var slide = scrolledbox.closest("section");
+//        var svgs = slide.querySelectorAll("svg");
+//        for(var iSvg=0; iSvg<svgs.length; iSvg++){
+//          svgs[iSvg].style.display = "none";
+//        }            
+//        window.clearTimeout(timeoutId);
+//      }
+//      timeoutId = window.setTimeout(function(){updateAnno(scrolledbox, maxCharCountPerLine, marginSize, fontSize);}, 200);
+//    };
+//    scrolledbox.addEventListener("scroll", delayfunc);  
+//  }
+//});//
--- quilt.js
+++ quilt.js
@@ -33,32 +33,44 @@ fs.readFile(options.project[0], 'utf8', function (err,source) {
         if(p1.includes("glsl")) {
           language = "glsl";
         }
-        patch.replaceAll(/^\+(.+)$/mg, (match, l1) => {
-          if(l1.startsWith("++")){
-            if(inSection){
-              slide += `
-         </script> </code></pre>
-      </section>`;
-            }
-            slide += `<section>
-        <h3>`;
-            slide += l1.substring(5);
-          //slide += p1;
-          slide += `</h3>
-                  <pre class="${language}"><code class="floating-annotations" data-trim> <script type="text/template">
+        const files = patch.split("\n+++ b/");
+        files.shift();
+        files.forEach(file => {
+          hunks = file.split(/[\r\n]-- [\r\n]/)[0].split("\n@@");
+          if(inSection){
+            slide += `
+       </script> </code></pre>
+    </section>`;
+          }
+          slide += `<section>
+      <h3>`;
+          slide += hunks[0];
+        slide += `</h3>
+                <pre class="${language}"><code class="floating-annotations" data-trim> <script type="text/template">
 `;
-          } else {
-//            slide += l1.replace(/^(\s*)([^\s]+)\/\/\$(.+)$/, (match, lead, code, comment) =>
-            slide += l1.replace(format, (match, lead, code, comment) => { 
-                const langComments = comment.split("˙HUN˙");
-                if(langComments.length < languageId) {
-                  comment = langComments[languageId];
-                } else {
-                  comment = langComments[0];
+          hunks.shift();
+          hunks.forEach( chunk => {
+            var hunk = "@@" + chunk;
+  //        patch.replaceAll(/\n@@.*\n([^]*)(\n@@|\n--)/g, (match, hunk, terminator) => {
+            hunk.replaceAll(/^(\+|-| )(.*)$/mg, (match, indicator, l1) => {
+  //            slide += l1.replace(/^(\s*)([^\s]+)\/\/\$(.+)$/, (match, lead, code, comment) =>
+              if(indicator == "-") {
+                slide += "<del>";
+              } else {
+                if(indicator == " ") {
+                  slide += "<ins>";
                 }
+              }
+              slide += l1.replace(format, (match, lead, code, comment) => { 
                 comment.replaceAll(/#([^#]*)#([^#]+)/g, (match, pattern, anno) => {
                   if(pattern == ""){
                     pattern = code;
+                  }
+                  const langAnnos = anno.split("˙HUN˙");
+                  if(langAnnos.length < languageId) {
+                    anno = langAnnos[languageId];
+                  } else {
+                    anno = langAnnos[0];
                   }
                   var m = code.match(new RegExp(pattern.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')));
                   code = code.slice(0, m.index) + 
@@ -67,11 +79,18 @@ fs.readFile(options.project[0], 'utf8', function (err,source) {
                     `</span>` + 
                     code.slice(m.index+m[0].length);
                 });
-              return lead + code;
-//                return lead + `<span comment="${comment.replace("; ","\n")}">` + code + `</span>`}
+                return lead + `<span comment="${comment.replace("; ","\n")}">` + code + `</span>`;
+              });
+              if(indicator == "-") {
+                slide += "</del>";
+              } else {
+                if(indicator == " ") {
+                  slide += "</ins>";
+                }
+              }              
+              slide += "\n";
             });
-          }
-          slide += "\n";
+          });
         });
         slide += `
          </script> </code></pre>
