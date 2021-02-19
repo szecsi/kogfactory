02e742e234fcca0b2bf81a4be7fac3d4cbd7e71e
szecsi <szecsi.laszlo@gmail.com>
2021-02-19 16:46:16 +0100

    vectors factoring

---
+++ projects/kog.010.050-vectors/qinit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -v
+
+wd=$(pwd)
+cd ../..
+var=`date +"%FORMAT_STRING"`
+now=`date +"%F_%R"`
+mv kogedge kogedge$now
+git clone -b empty --single-branch https://github.com/szecsi/kogedge.git
+cd kogedge
+git checkout -b quilted
+# apply all browser patches
+echo $wd"/../../patches/kog.010.010.*.patch" | xargs -I % bash -c "git am %*"
+# apply all webgl patches
+echo $wd"/../../patches/kog.010.020.*.patch" | xargs -I % bash -c "git am %*"
+# apply webglmath patch
+echo $wd"/../../patches/kog.010.050.0010-WebGLMath.patch" | xargs -I % bash -c "git am %*"
